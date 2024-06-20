2668012e79e8f59a49b44d440c1a7ca8c2b38d24
YangYuan <yangyuan@kuaishou.com>
2024-06-20 22:18:44 +0800

    add image

--- image-cropper/src/App.tsx
+++ image-cropper/src/App.tsx
@@ -2,11 +2,12 @@
  * @Author: yangyuan
  * @Description: 
  * @Date: 2024-06-20 14:42:04
- * @LastEditTime: 2024-06-20 20:43:30
+ * @LastEditTime: 2024-06-20 22:16:42
  */
-import './css/custom.css';
 import Layout from './components/layout/Layout';
 import { CroppersWizard } from './components/CroppersWizard'
+import './app.scss';
+import 'react-advanced-cropper/dist/style.css';
 
 function App() {
 
--- image-cropper/vite.config.ts
+++ image-cropper/vite.config.ts
@@ -1,7 +1,15 @@
+/*
+ * @Author: yangyuan
+ * @Description: 
+ * @Date: 2024-06-20 14:42:04
+ * @LastEditTime: 2024-06-20 22:14:38
+ */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+  ],
 })
